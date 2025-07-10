da90aa42c354667b7e566231c45d1f025ad497c5
SuzuSys <sigmatics@outlook.jp>
2025-07-10 19:29:38 +0900

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,45 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <filesystem>
+#include <iostream>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::ContextSettings settings;
+	settings.antiAliasingLevel = 4;
+	sf::Window window(sf::VideoMode({ 800, 600 }), "BNW", sf::Style::Default, sf::State::Windowed);
 
-    while (window.isOpen())
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+
+	window.setActive(true);
+
+    // load resources, initialize the OpenGL states, ...
+
+    bool running = true;
+    while (running)
     {
+        // handle events
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
             {
-                window.close();
+                running = false;
+            }
+            else if (const auto* resized = event->getIf<sf::Event::Resized>()) {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, resized->size.x, resized->size.y);
             }
         }
+        // clear the buffers
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        window.clear();
+        // draw...
+
         window.display();
     }
+
+    // release resources...
 }
